676ee51a9d3072d928f8ce0a0ce2f101bdd06bc8
scatyf3 <scatyf3@gmail.com>
2026-09-13 21:54:29 -0400

    sync

--- 128. Longest Consecutive Sequence/sol.py
+++ 128. Longest Consecutive Sequence/sol.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # O(n)
-        # step1: build a hash table key:elem value: next elem, graph; find start
-        # step2: iterate through hash table, key,record max len and visited node
-        # wait what we can build set using api directly
-        s = set(nums)
-        # 第一遍:挑出所有链头, 这里的diff是需要所有start
-        starts = [x for x in s if x - 1 not in s]
-        # 第二遍:每条链走一次
-        best = 0
-        for x in starts:
-            y = x
-            while y + 1 in s:
-                y += 1
-            best = max(best, y - x + 1)
-        return best
-    
+        st = set(nums)
+        starts = []
+        for elem in st:
+            if elem-1 not in st:
+                starts.append(elem)
+        longest = 0
+        
+        for start in starts:
+            cur_len=1
+            while start+1 in st:
+                cur_len+=1
+                start+=1
+            longest=max(cur_len,longest)
+        return longest
